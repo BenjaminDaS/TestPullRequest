db825386e197d0ebc2cd51c965d2994a8e2b94e8
Da Silva Benjamin <benjiji3101@gmail.com>
2024-01-22 14:12:55 +0100

    test

---
+++ src/test/java/com/example/testaction/TestActionApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.testaction;
+
+import jdk.jshell.spi.ExecutionControl;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void test() {
+        int a = 1;
+    }
+
+    @Test
+    void test2() throws Exception {
+        if(1 == 1) {
+            throw new Exception();
+        }
+    }
+}
